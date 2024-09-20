cd8378154c37886df35fa27b67718fc6067a4a42
Luca Bruno <lucabrun@amazon.com>
2024-09-20 22:51:22 +0100

    tf: Create user data for EC2 instance
    Learned:
    - $HOME is missing
    - swap is likely need for low-memory instances
    - rust takes forever to compile stuff

--- tf/main.tf
+++ tf/main.tf
@@ -75,21 +75,77 @@ data "aws_availability_zones" "any" {
 module "learning_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "learning_vpc"
   cidr = "10.0.0.0/16"
 
   azs             = data.aws_availability_zones.any.zone_ids
   private_subnets = ["10.0.1.0/24"]
   public_subnets  = ["10.0.101.0/24"]
 
+  map_public_ip_on_launch = true
   create_igw = true
 }
 
 /* EC2 */
 
+variable "ssh_pub_key_file" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
+resource "aws_key_pair" "learning_key" {
+  key_name   = "learning_key"
+  public_key = file(var.ssh_pub_key_file)
+}
+
+resource "aws_iam_instance_profile" "learning_ec2_profile" {
+  role = aws_iam_role.learning_ec2_role.name
+}
+
 resource "aws_launch_template" "learning_template" {
   image_id      = data.aws_ami.latest_amzn.id
   instance_type = "t4g.nano"
+  user_data = filebase64("${path.module}/user_data/user_data.sh")
+  key_name = aws_key_pair.learning_key.key_name
+  vpc_security_group_ids = [aws_security_group.learning_sg.id]
+
+  iam_instance_profile {
+    arn = aws_iam_instance_profile.learning_ec2_profile.arn
+  }
+}
+
+resource "aws_iam_role" "learning_ec2_role" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+  ]
+}
+
+resource "aws_security_group" "learning_sg" {
+  vpc_id = module.learning_vpc.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["80.233.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_autoscaling_group" "learning_asg" {
@@ -97,9 +153,13 @@ resource "aws_autoscaling_group" "learning_asg" {
   max_size = 1
   min_size = 0
   desired_capacity = var.asg_desired_capacity
+  instance_refresh {
+    strategy = "Rolling"
+  }
 
   launch_template {
     id      = aws_launch_template.learning_template.id
     version = aws_launch_template.learning_template.latest_version
   }
 }
+
