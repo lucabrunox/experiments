cdefa206d9756c2d5c124e7f0fbe5b9ee89318e6
Luca Bruno <lucabrun@amazon.com>
2024-10-02 22:19:39 +0100

    Better name for the repo

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-## Learning and sharing random stuff
+## Experiments and random stuff
 
-This repo contains step-by-step creationg of low-level Terraform + K8s + other stuff for learning purposes.
+This repo contains some notes about setting up K8s, ingresses and apps in a non-conventional way.
+
+For example:
+- Using K8s on EC2 without EKS
+- Using a raw Nginx config without using K8s ingress.
 
 ### Day 1: Set up Terraform with a remote backend
 
-Commit: https://github.com/lucabrunox/learning/tree/cd8378154c378
+Commit: https://github.com/lucabrunox/experiments/tree/cd8378154c378
 
 Define the AWS region that will be used for all the commands:
 
@@ -15,7 +19,7 @@ export AWS_REGION=eu-west-1
 Create an S3 bucket for terraform state:
 
 ```bash
-aws s3api create-bucket --acl private --bucket learning-12345-terraform --create-bucket-configuration LocationConstraint=$AWS_REGION
+aws s3api create-bucket --acl private --bucket experiments-12345-terraform --create-bucket-configuration LocationConstraint=$AWS_REGION
 ```
 
 Initialize:
@@ -24,8 +28,8 @@ Initialize:
 cd tf
 
 cat <<EOF > backend.conf
-bucket = "learning-12345-terraform"
-key    = "learning/terraform.tfstate"
+bucket = "experiments-12345-terraform"
+key    = "experiments/terraform.tfstate"
 region = "$AWS_REGION"
 EOF
 
@@ -46,17 +50,12 @@ Use asg_desired_capacity=0 to tear down the cluster.
 
 ### Day 2: Kubernetes single-node cluster on EC2 with kubeadm
 
-Commit: https://github.com/lucabrunox/learning/tree/9cc3ac81d7f
+Commit: https://github.com/lucabrunox/experiments/tree/9cc3ac81d7f
 
-Using a raw K8s instead of EKS to learn some low-level details. Some interesting facts:
+Using a raw K8s on EC2 instead of EKS, using Flannel instead of AWS CNI. Some interesting facts:
 
 - It takes 2 minutes and 20 second until all containers are in Running state.
 - A t4g.medium is needed to run a cluster. Using a t4g.nano with swap is not enough because the apiserver/etcd will keep timing out.
-- CoreDNS and kube-proxy addons are installed by default.
-- The advertising IP is coming from `ip route` and it coincides with the private IP of the instance rather than the public one.
-- Explanations of K8s networking:
-  - https://mvallim.github.io/kubernetes-under-the-hood/documentation/kube-flannel.html
-  - https://www.redhat.com/sysadmin/kubernetes-pods-communicate-nodes
 
 SSH into the EC2 instance and run crictl and kubectl commands to inspect the cluster:
 
@@ -69,49 +68,40 @@ export KUBECONFIG=/etc/kubernetes/admin.conf
 kubectl get all -A
 ```
 
-If the cluster is not up check the instance init logs:
+To check init logs:
 
 ```bash
 sudo cat /var/log/cloud-init-output.log
 ```
 
-### Day 3: A Django frontend with GH action to build a Docker image and push to ECR
+### Day 3: Sample app with Docker pushed to ECR
 
-Commit: https://github.com/lucabrunox/learning/tree/5216dfe5efd6
+Commit: https://github.com/lucabrunox/experiments/tree/5216dfe5efd6
 
 Set up following https://docs.djangoproject.com/en/5.1/intro/tutorial01/ with `django-admin startproject mysite`.
 
-The Dockerfile is self-explanatory. To try it out:
+The Dockerfile is self-explanatory:
 
 ```bash
 docker run -p 8000:8000 --rm -it $(docker build -q .)
 ```
 
-Then open http://localhost:8000
-
-To test GH actions I've set up act to run in a Docker, so that it doesn't need to be installed:
+To test GH actions I've set up act to run in a Docker, which seems to work fine:
 
 ```bash
 ./test_gh.sh
 ```
 
-Which in turn creates the frontend Docker, yay!
+### Day 4: Deploy the app in K8s using the ECR image
 
-The GH also contains a job to push to ECR, which is not tested locally.
+Commit: https://github.com/lucabrunox/experiments/tree/5216dfe5efd6
 
-### Day 4: Deploy the Django app in K8s using the ECR image
+We're using a CronJob to get the ECR creds from the node metadata.
 
-Commit: https://github.com/lucabrunox/learning/tree/5216dfe5efd6
-
-Needless to say that without EKS it's more complicated, but worth the learnings.
-
-Learnings:
-- Using CronJob to get AWS creds from the node, login to ECR, and store the secret for pulling images.
-- CronJob doesn't start immediately, need to wait a minute.
+Some notes:
 - Need to untaint control plane node in other to schedule pods.
-- Need to build the frontend image for ARM, obviously.
-- Python app fails because it can't find the UTC timezone, needs tzdata.
-- Cannot change the matching labels of a K8s deployment.
+- As we use t4g changed the GH build to ARM.
+- Python requirements also need tzdata.
 
 At the end we're able to execute the following kubectl on the EC2 instance to deploy the app and watch it working:
 
@@ -124,12 +114,10 @@ curl $(kubectl get svc frontend -o=jsonpath='{.spec.clusterIP}'):8000
 
 ### Day 5: Expose service via NLB and NodePort
 
-Commit: https://github.com/lucabrunox/learning/tree/f03d8449f869
+Commit: https://github.com/lucabrunox/experiments/tree/f03d8449f869
 
-Publicly exposing the service via NLB learnings:
-- Allowed node ports only from 30000
-- NLB security group must be configured for each listener port
-- NLBs need at least 2 subnets for redundancy
+Some notes:
+- Configured NLB with the security group in 2 subnets.
 - Django has an ALLOWED_HOSTS config to prevent Host header attacks
 - Django detects a tty when logging to stdout
 
@@ -137,5 +125,5 @@ Publicly exposing the service via NLB learnings:
 kubectl apply -f k8s/ecr-credentials.yaml
 kubectl apply -f frontend/k8s/manifest.yaml
 
-curl http://$(terraform output --raw learning_nlb_dns_name)
+curl http://$(terraform output --raw experiments_nlb_dns_name)
 ```
--- tf/main.tf
+++ tf/main.tf
@@ -44,7 +44,7 @@ data "aws_availability_zones" "any" {
 
 /* VPC, trying the github module */
 
-module "learning_vpc" {
+module "experiments_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   cidr = "10.0.0.0/16"
@@ -59,17 +59,17 @@ module "learning_vpc" {
 
 /* EC2 user data */
 
-resource "random_id" "learning_user_data_bucket_id" {
+resource "random_id" "experiments_user_data_bucket_id" {
   byte_length = 8
 }
 
-resource "aws_s3_bucket" "learning_s3_user_data" {
-  bucket_prefix = "learning-"
+resource "aws_s3_bucket" "experiments_s3_user_data" {
+  bucket_prefix = "experiments-"
   force_destroy = true
 }
 
-resource "aws_s3_object" "learning_user_data" {
-  bucket = aws_s3_bucket.learning_s3_user_data.bucket
+resource "aws_s3_object" "experiments_user_data" {
+  bucket = aws_s3_bucket.experiments_s3_user_data.bucket
   for_each = fileset("${path.module}/user_data/", "*")
   key = each.value
   source = "${path.module}/user_data/${each.value}"
@@ -78,11 +78,11 @@ resource "aws_s3_object" "learning_user_data" {
 
 /* EC2 role */
 
-resource "aws_iam_instance_profile" "learning_ec2_profile" {
-  role = aws_iam_role.learning_ec2_role.name
+resource "aws_iam_instance_profile" "experiments_ec2_profile" {
+  role = aws_iam_role.experiments_ec2_role.name
 }
 
-resource "aws_iam_role" "learning_ec2_role" {
+resource "aws_iam_role" "experiments_ec2_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -100,8 +100,8 @@ resource "aws_iam_role" "learning_ec2_role" {
   ]
 }
 
-resource "aws_iam_role_policy" "learning_ec2_inline_policy" {
-  role = aws_iam_role.learning_ec2_role.id
+resource "aws_iam_role_policy" "experiments_ec2_inline_policy" {
+  role = aws_iam_role.experiments_ec2_role.id
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -112,8 +112,8 @@ resource "aws_iam_role_policy" "learning_ec2_inline_policy" {
         ]
         Effect = "Allow"
         Resource = [
-          aws_s3_bucket.learning_s3_user_data.arn,
-          "${aws_s3_bucket.learning_s3_user_data.arn}/*"
+          aws_s3_bucket.experiments_s3_user_data.arn,
+          "${aws_s3_bucket.experiments_s3_user_data.arn}/*"
         ]
       },
       {
@@ -123,7 +123,7 @@ resource "aws_iam_role_policy" "learning_ec2_inline_policy" {
         ]
         Effect = "Allow"
         Resource = [
-          module.learning_ecr_frontend.repository_arn
+          module.experiments_ecr_frontend.repository_arn
         ]
       },
       {
@@ -137,13 +137,13 @@ resource "aws_iam_role_policy" "learning_ec2_inline_policy" {
 
 /* EC2 security */
 
-resource "aws_key_pair" "learning_key" {
-  key_name   = "learning_key"
+resource "aws_key_pair" "experiments_key" {
+  key_name   = "experiments_key"
   public_key = file(var.ssh_pub_key_file)
 }
 
-resource "aws_security_group" "learning_sg" {
-  vpc_id = module.learning_vpc.vpc_id
+resource "aws_security_group" "experiments_sg" {
+  vpc_id = module.experiments_vpc.vpc_id
 
   ingress {
     from_port   = 22
@@ -157,7 +157,7 @@ resource "aws_security_group" "learning_sg" {
     from_port   = 30000
     to_port     = 30000
     protocol    = "tcp"
-    security_groups = module.learning_nlb.security_group_id != null ? [module.learning_nlb.security_group_id] : []
+    security_groups = module.experiments_nlb.security_group_id != null ? [module.experiments_nlb.security_group_id] : []
   }
 
   egress {
@@ -184,10 +184,10 @@ locals {
   user_data_hash = sha1(join("", [for f in fileset(path.module, "user_data/**") : filesha1("${path.module}/${f}")]))
 }
 
-resource "aws_launch_template" "learning_template" {
+resource "aws_launch_template" "experiments_template" {
   image_id      = data.aws_ami.latest_amzn.id
   instance_type = "t4g.medium"
-  depends_on = [aws_s3_object.learning_user_data]
+  depends_on = [aws_s3_object.experiments_user_data]
 
   user_data = base64encode(<<EOF
 #!/bin/bash
@@ -195,14 +195,14 @@ export HOME=/root
 mkdir /root/init
 cd /root/init
 # trigger: ${local.user_data_hash}
-aws s3 cp --recursive s3://${aws_s3_bucket.learning_s3_user_data.bucket}/ ./
+aws s3 cp --recursive s3://${aws_s3_bucket.experiments_s3_user_data.bucket}/ ./
 chmod +x init.sh
 ./init.sh && rm -rf /root/init # keep init files around for debugging
 EOF
   )
 
-  key_name = aws_key_pair.learning_key.key_name
-  vpc_security_group_ids = [aws_security_group.learning_sg.id]
+  key_name = aws_key_pair.experiments_key.key_name
+  vpc_security_group_ids = [aws_security_group.experiments_sg.id]
   block_device_mappings {
     # swap
     device_name = "/dev/sdf"
@@ -214,12 +214,12 @@ EOF
   }
 
   iam_instance_profile {
-    arn = aws_iam_instance_profile.learning_ec2_profile.arn
+    arn = aws_iam_instance_profile.experiments_ec2_profile.arn
   }
 }
 
-resource "aws_autoscaling_group" "learning_asg" {
-  vpc_zone_identifier = module.learning_vpc.public_subnets
+resource "aws_autoscaling_group" "experiments_asg" {
+  vpc_zone_identifier = module.experiments_vpc.public_subnets
   max_size = 1
   min_size = 0
   desired_capacity = var.asg_desired_capacity
@@ -227,22 +227,22 @@ resource "aws_autoscaling_group" "learning_asg" {
     strategy = "Rolling"
   }
 
-  target_group_arns = [for group in module.learning_nlb.target_groups: group.arn]
+  target_group_arns = [for group in module.experiments_nlb.target_groups: group.arn]
 
   launch_template {
-    id      = aws_launch_template.learning_template.id
-    version = aws_launch_template.learning_template.latest_version
+    id      = aws_launch_template.experiments_template.id
+    version = aws_launch_template.experiments_template.latest_version
   }
 }
 
 /* NLB */
 
-module "learning_nlb" {
+module "experiments_nlb" {
   source = "terraform-aws-modules/alb/aws"
   create = var.nlb_enabled
   load_balancer_type = "network"
-  vpc_id = module.learning_vpc.vpc_id
-  subnets = aws_autoscaling_group.learning_asg.vpc_zone_identifier
+  vpc_id = module.experiments_vpc.vpc_id
+  subnets = aws_autoscaling_group.experiments_asg.vpc_zone_identifier
   enable_deletion_protection = false
 
   listeners = [
@@ -250,14 +250,14 @@ module "learning_nlb" {
       port = 80
       protocol = "TCP"
       forward = {
-        target_group_key = "learning"
+        target_group_key = "experiments"
       }
     }
   ]
 
   target_groups = {
-    learning = {
-      name_prefix = "learn-"
+    experiments = {
+      name_prefix = "exp-"
       protocol = "TCP"
       port = 30000
       target_type = "instance"
@@ -283,17 +283,16 @@ module "learning_nlb" {
   security_group_egress_rules = {
     all = {
       ip_protocol = "-1"
-      cidr_ipv4   = module.learning_vpc.vpc_cidr_block
+      cidr_ipv4   = module.experiments_vpc.vpc_cidr_block
     }
   }
 }
 
 /* ECR */
 
-module "learning_ecr_frontend" {
+module "experiments_ecr_frontend" {
   source = "terraform-aws-modules/ecr/aws"
-
-  repository_name = "learning-frontend"
+  repository_name = "experiments-frontend"
 
   repository_lifecycle_policy = jsonencode({
     rules = [
@@ -316,7 +315,7 @@ module "learning_ecr_frontend" {
 
 /* GitHub */
 
-resource "aws_iam_policy" "learning_github_ecr" {
+resource "aws_iam_policy" "experiments_github_ecr" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -331,7 +330,7 @@ resource "aws_iam_policy" "learning_github_ecr" {
         ]
         Effect = "Allow"
         Resource = [
-          module.learning_ecr_frontend.repository_arn
+          module.experiments_ecr_frontend.repository_arn
         ]
       },
       {
@@ -343,19 +342,19 @@ resource "aws_iam_policy" "learning_github_ecr" {
   })
 }
 
-module "learning_github_oidc" {
+module "experiments_github_oidc" {
   source  = "terraform-module/github-oidc-provider/aws"
   version = "~> 1"
 
   create_oidc_provider = true
   create_oidc_role     = true
 
-  repositories              = ["lucabrunox/learning"]
-  oidc_role_attach_policies = [aws_iam_policy.learning_github_ecr.arn]
+  repositories              = ["lucabrunox/experiments"]
+  oidc_role_attach_policies = [aws_iam_policy.experiments_github_ecr.arn]
 }
 
 /* Outputs */
 
-output "learning_nlb_dns_name" {
-  value = module.learning_nlb.dns_name
+output "experiments_nlb_dns_name" {
+  value = module.experiments_nlb.dns_name
 }
